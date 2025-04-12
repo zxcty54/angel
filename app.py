bd0c2eb65df393102d6b2e8044c03b65ce4b8bde
zxcty54 <nitesh.skyhigh@gmail.com>
2025-04-12 10:25:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import pyotp
 from dotenv import load_dotenv
 from SmartApi.smartConnect import SmartConnect
   # pip install angel-one-smartapi
-from Cryptodome.PublicKey import RSA
+from Crypto.PublicKey import RSA  ✅
 from time import sleep
 
 # Load environment variables
