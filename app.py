3cd78d37d6db290346faa015025e9bcc36084cd2
zxcty54 <nitesh.skyhigh@gmail.com>
2025-04-12 10:14:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ import logging
 import json
 import pyotp
 from dotenv import load_dotenv
-from smartapi import SmartConnect  # pip install angel-one-smartapi
+from smartapi.smartConnect import SmartConnect
+  # pip install angel-one-smartapi
 from Cryptodome.PublicKey import RSA
 from time import sleep
 
